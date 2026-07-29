0cc4d9224656a6e8b238fa90c4dc943ed4350cc8
MaryN333 <sidyakina@gmail.com>
2026-07-29 15:21:39 +0200

    feat: implement Redis integration - add CityCountry and Language DTOs for Redis storage - add prepareRedisClient() for Redis connection - add transformData() to convert City entity to CityCountry - add pushToRedis() to store transformed data in Redis

--- src/main/java/cz/wz/marysidy/world/App.java
+++ src/main/java/cz/wz/marysidy/world/App.java
@@ -1,13 +1,20 @@
 package cz.wz.marysidy.world;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.wz.marysidy.world.dao.CityDAO;
 import cz.wz.marysidy.world.dao.CountryDAO;
 import cz.wz.marysidy.world.domain.City;
 import cz.wz.marysidy.world.domain.Continent;
 import cz.wz.marysidy.world.domain.Country;
+import cz.wz.marysidy.world.domain.CountryLanguage;
+import cz.wz.marysidy.world.redis.CityCountry;
+import cz.wz.marysidy.world.redis.Language;
 import cz.wz.marysidy.world.util.MySessionFactory;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStringCommands;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -16,14 +23,20 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class App {
     private final SessionFactory sessionFactory;
     private final CountryDAO countryDAO;
     private final CityDAO cityDAO;
+    private final RedisClient redisClient;
+    private final ObjectMapper mapper;
+
 
     // Test raw Hibernate query to verify connection
     private void testConnection(Session session) {
+        System.out.println("Connected to DB!!!");
         List<Country> countries = session.createQuery("from Country", Country.class)
                 .setMaxResults(5).getResultList();
         System.out.println("Found countries (first 5):");
@@ -94,10 +107,63 @@ public class App {
         return allCities;
     }
 
+    private List<CityCountry> transformData(List<City> cities) {
+        return cities.stream().map(city -> {
+            CityCountry res = new CityCountry();
+            res.setId(city.getId());
+            res.setName(city.getName());
+            res.setPopulation(city.getPopulation());
+            res.setDistrict(city.getDistrict());
+
+            Country country = city.getCountry();
+            res.setAlternativeCountryCode(country.getAlternativeCode());
+            res.setContinent(country.getContinent());
+            res.setCountryCode(country.getCode());
+            res.setCountryName(country.getName());
+            res.setCountryPopulation(country.getPopulation());
+            res.setCountryRegion(country.getRegion());
+            res.setCountrySurfaceArea(country.getSurfaceArea());
+            Set<CountryLanguage> countryLanguages = country.getLanguages();
+            Set<Language> languages = countryLanguages.stream().map(cl -> {
+                Language language = new Language();
+                language.setLanguage(cl.getLanguage());
+                language.setOfficial(cl.getOfficial());
+                language.setPercentage(cl.getPercentage());
+                return language;
+            }).collect(Collectors.toSet());
+            res.setLanguages(languages);
+
+            return res;
+        }).collect(Collectors.toList());
+    }
+
+    private RedisClient prepareRedisClient() {
+        RedisClient redisClient = RedisClient.create(RedisURI.create("localhost", 6379));
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            System.out.println("\nConnected to Redis\n");
+        }
+        return redisClient;
+    }
+
+    private void pushToRedis(List<CityCountry> data) {
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            RedisStringCommands<String, String> sync = connection.sync();
+            for (CityCountry cityCountry : data) {
+                try {
+                    sync.set(String.valueOf(cityCountry.getId()), mapper.writeValueAsString(cityCountry));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public App() {
         this.sessionFactory = MySessionFactory.getSessionFactory();
         this.countryDAO = new CountryDAO(sessionFactory);
         this.cityDAO = new CityDAO(sessionFactory);
+        this.redisClient = prepareRedisClient();
+        this.mapper = new ObjectMapper();
     }
 
     public static void main(String[] args) {
@@ -110,21 +176,32 @@ public class App {
     public void run() {
         try (Session session = sessionFactory.getCurrentSession()) {
             Transaction transaction = session.beginTransaction();
-            System.out.println("Connected to DB!!!");
 
 //            testConnection(session);
 
 //            testCountryDAO();
 
-            System.out.println("Loading all cities from DB...");
+            System.out.println("\nLoading all cities from DB...");
             List<City> cities = fetchCities();
             System.out.println("Total cities loaded: " + cities.size());
 
+            System.out.println("\nTransforming data for Redis...");
+            List<CityCountry> cityCountries = transformData(cities);
+            System.out.println("Transformed " + cityCountries.size() + " records.");
+
             transaction.commit();
+
+            System.out.println("\nPushing data to Redis...");
+            pushToRedis(cityCountries);
+            System.out.println("Data successfully pushed to Redis!");
+
         } catch (Exception e) {
             System.err.println("Something wrong!!!");
             e.printStackTrace();
         } finally {
+            if (redisClient != null) {
+                redisClient.shutdown();
+            }
             MySessionFactory.shutdown();
         }
     }
